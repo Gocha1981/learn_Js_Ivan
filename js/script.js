acb4d30ca0e00e8ff1c332f765f97105899ef1d3
Gocha <example@yahoo.com>
2022-03-13 23:33:19 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// alert(result);
+
+// const answer = +prompt("Вам есть 18?", "");
+// console.log(answer);10
+
+const answers = [];
+answers[0] = prompt("Как ваше имя?", "");
+answers[1] = prompt("Как ваше фамилия?", "");
+answers[2] = prompt("Сколько вам лет?", "");
+
+console.log(answers);
